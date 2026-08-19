3994648346d800fd425ecb754f06572fff36d3a6
Mateus Scapin Belussi <scapinbelussimateus@gmail.com>
2026-08-19 11:45:26 -0300

    envio de projeto

--- assets/configurar-git.bat
+++ assets/configurar-git.bat
@@ -1,5 +1,5 @@
 @echo off
-git config user.name "Nome do Aluno"
-git config user.email "email@aluno.com"
+git config user.name "Mateus Scapin Belussi"
+git config user.email "scapinbelussimateus@gmail.com"
 echo Git configurado com sucesso para este projeto!
 pause
