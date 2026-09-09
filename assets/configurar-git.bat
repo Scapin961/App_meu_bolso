21f78daf3ec11f88cbf9d9040cca8b784e627296
Scapin961 <scapinbelussimateus@gmail.com>
2026-09-09 11:50:49 -0300

    Atualizando Repositório

--- assets/configurar-git.bat
+++ assets/configurar-git.bat
@@ -1,5 +1,5 @@
 @echo off
-git config user.name "Mateus Scapin Belussi"
+git config user.name "Scapin961"
 git config user.email "scapinbelussimateus@gmail.com"
 echo Git configurado com sucesso para este projeto!
 pause
